27fbc84b10a834fe6b639cbfed336246e7c66547
Varvara Pankova <varvarapankova@mail.ru>
2020-12-25 21:25:53 +0300

    25.12.2020

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import guideRoutes from './routes/guide.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use('/guide', guideRoutes);
+
+app.get('/', (req, res)=>res.send('Hello from Homepage.'));
+
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
